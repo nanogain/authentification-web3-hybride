449d5df641a162e50c42678682a9e7a00a224943
nanogain <151880349+nanogain@users.noreply.github.com>
2026-01-05 23:24:33 +0100

    Add files via upload

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
+const authMiddleware = require('../middleware/auth');
 const userCtrl = require('../controllers/user');
 
 router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
+router.get('/dashboard', authMiddleware, userCtrl.dashboard);
+router.post('/web3/login', userCtrl.web3Login);
 
 module.exports = router;
