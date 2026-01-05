16356d9b0c82f1e57accec750f1ed625efe862c7
nanogain <151880349+nanogain@users.noreply.github.com>
2026-01-05 23:23:37 +0100

    Add files via upload

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  email: { type: String, },
+  password: { type: String, },
+  pseudo: { type: String, },
+  walletAddress: { type: String, lowercase: true, },
+  nonce: { type: String, default: () => Math.floor(Math.random() * 1000000).toString() }
 });
 
 userSchema.plugin(uniqueValidator);
