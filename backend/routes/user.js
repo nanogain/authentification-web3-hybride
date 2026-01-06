d3916f5e3745d6b34868f6c4b0ff09ec18831831
nanogain <151880349+nanogain@users.noreply.github.com>
2026-01-06 17:18:01 +0100

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,5 +7,6 @@ router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 router.get('/dashboard', authMiddleware, userCtrl.dashboard);
 router.post('/web3/login', userCtrl.web3Login);
+router.post('/web3/liaison', userCtrl.web3Liaison);
 
 module.exports = router;
